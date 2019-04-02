cdf01c82c94695af2e1920939de89680d71f9410
diego-amador <diego.amador@upr.edu>
2019-04-02 10:51:52 -0400

    GUI UPDATES FROM ALL MESSAGES IN THE DB

--- appjs/chat.js
+++ appjs/chat.js
@@ -1,18 +1,56 @@
-angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope',
-    function($http, $log, $scope) {
+angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope','$rootScope', '$location',
+    function($http, $log, $scope, $rootScope, $location) {
         var thisCtrl = this;
 
         this.messageList = [];
         this.counter  = 2;
         this.newText = "";
-
+        $rootScope.prueba = "";
         this.loadMessages = function(){
             // Get the messages from the server through the rest api
-            thisCtrl.messageList.push({"id": 1, "text": "Hola Mi Amigo", "author" : "Bob",
-            "like" : 4, "nolike" : 1});
-            thisCtrl.messageList.push({"id": 2, "text": "Hello World", "author": "Joe",
-                "like" : 11, "nolike" : 12});
+          
 
+            // First set up the url for the route
+            var url = "http://127.0.0.1:5000/kheApp/dev/messages";
+
+            // Now set up the $http object
+            // It has two function call backs, one for success and one for error
+            $http.get(url).then(// success call back
+                function (response){
+                // The is the sucess function!
+                // Copy the list of parts in the data variable
+                // into the list of parts in the controller.
+
+                    console.log("response: " + JSON.stringify(response));
+
+                    thisCtrl.messageList = response.data.Messages;
+                    $rootScope.prueba = "Probando";
+            }, // error callback
+            function (response){
+                // This is the error function
+                // If we get here, some error occurred.
+                // Verify which was the cause and show an alert.
+                console.log("Err response: " + JSON.stringify(response));
+
+                var status = response.status;
+                if (status == 0){
+                    alert("No hay conexion a Internet");
+                }
+                else if (status == 401){
+                    alert("Su sesion expiro. Conectese de nuevo.");
+                }
+                else if (status == 403){
+                    alert("No esta autorizado a usar el sistema.");
+                }
+                else if (status == 404){
+                    alert("No se encontro la informacion solicitada.");
+                }
+                else {
+                    alert("Error interno del sistema.");
+                }
+            });
+
+            
             $log.error("Message Loaded: ", JSON.stringify(thisCtrl.messageList));
         };
 
@@ -24,6 +62,11 @@ angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope
             thisCtrl.messageList.unshift({"id": nextId, "text" : msg, "author" : author, "like" : 0, "nolike" : 0});
             thisCtrl.newText = "";
         };
+
+
+
+        
+        
 
         this.loadMessages();
 }]);
